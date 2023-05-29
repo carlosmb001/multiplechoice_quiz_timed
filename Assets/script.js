a269980025c73fd508c725e10c4fb958706af205
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-05-28 23:11:59 -0700

    added questionaire

--- Assets/script.js
+++ Assets/script.js
@@ -3,35 +3,42 @@
 var questions = [
     {
       question: "Javascript is an _______ language?",
-      answers: ["Object-Oriented", "Object-Based", "Procedural", "None of the Above"],
-      answer: 0
+      choices: ["Object-Oriented", "Object-Based", "Procedural", "None of the Above"],
+      answer: "Object-Oriented"
     },
     {
       question: "Which of the following methods is used to access HTML elements using Javascript?",
-      answers: ["getElementById()", "getElementsByClassName()", "Both A and B", "None of the Above"],
-      answer: 2
+      choices: ["getElementById()", "getElementsByClassName()", "Both A and B", "None of the Above"],
+      answer: "Both A and B"
     },
     {
       question: "Which of the following keywords is used to define a variable in Javascript?",
-      answers: ["var", "let", "Both A and B", "None of the Above"],
-      answer: 2
+      choices: ["var", "let", "Both A and B", "None of the Above"],
+      answer: "Both A and B"
     },
     {
       question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
-      answers: ["Throws an error", "Ignores the statement", "Gives a warning", "None of the Above"],
-      answer: 1
+      choices: ["Throws an error", "Ignores the statement", "Gives a warning", "None of the Above"],
+      answer: "Ignores the statement"
     }
   ];
   
-  counter = 0;
-  timer = 60000;
+  var score = 0;
+  var seconds = 60;
+  var questionIndex = 0;
+  var choices = 4;
+  var currentQuestion = questions[questionIndex];
+
 
   //get the information about the question, choices and answer 
   var startEl = document.getElementById("start");
   var timerEl = document.getElementById("timer");
   var highScoreEl = document.getElementById("highscore");
   var questionEl = document.getElementById("questions");
+  var questionTitleEl = document.getElementById("question-title");
   var choiceEl = document.getElementsByClassName("choice");
+  var scoreEl = document.getElementById("final-score");
+
 
   function clearIntro(){
   startEl.style.display = "none";
@@ -40,52 +47,56 @@ var questions = [
   }
 
   function start(){
-    for (var i = 0; i < questions.length; i++) {
-      questionEl.textContent = questions[i].question;
-      for (var j = 0; j < questions[i].answers.length; j++){
-        choiceEl.textContent = questions[i].answers[j];
+      questionTitleEl.textContent = currentQuestion.question;
+      currentQuestion = questions[questionIndex];
+
+      for (var i = 0; i < choices; i++){
+        //choiceEl.textContent = currentQuestion.answers[i];
+        var button = document.getElementById("answer" + i);
+        button.textContent = currentQuestion.choices[i];
+        button.addEventListener("click", checkAnswer);
       }
+
+    }
+
+  function checkAnswer(event){
+    var correctAnswer = currentQuestion.answer;
+    var selectedButton = event.target.textContent;
+
+    if (selectedButton === correctAnswer) {
+      score++;
+    }
+    else {
+      seconds -= 10;
+    }
+    if (questionIndex < questions.length -1) {
+    
+    questionIndex++;
+    start();
+    } else {
+      seconds = 0;
+      scoreEl.textContent = "You got " + score +" answers correct" ;
+      questionEl.style.display = "none";
+      timerEl.style.display = "none";
+      return;
     }
   }
 
-  //fuction to start the time
+  // function to start the timer
   function countdown(timerEl) {
-    var seconds = 120;
     var intervalId = setInterval(function() {
       seconds--;
       timerEl.textContent = seconds;
       if (seconds === 0) {
         clearInterval(intervalId);
       }
+      else{
+        return;
+      }
     }, 1000);
-    
+
   }
 
-  /* questionEl.textContent = questions[currentQuestionIndex].question;
-
-
-  var choicesEl = document.getElementById("choices");
-  choicesEl.textContent = "";
-
-  for (var i = 0; i < questions[currentQuestionIndex].answerChoices.length; i++) {
-    var choiceEl = document.createElement("button");
-    choiceEl.textContent = questions[currentQuestionIndex].answerChoices[i];
-    choiceEl.setAttribute("class", "choice");
-    choicesEl.appendChild(choiceEl);
-    choiceEl.addEventListener("click", checkAnswer);
-  }
-
-
-  //questionEl.textContent = questions[currentQuestionIndex].question;
-
-
-  //var choicesEl = document.getElementById("choices");
- // choicesEl.textContent = "";
-*/
-
-
-  // event listener for answer choices, checks if the answer is correct, and updates
-  // will start the question, the timer, and the prompt message should disappear
 
   // this will start the question and prompt message
   startEl.addEventListener("click",function() {
